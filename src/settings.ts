ccc56dded5f562c4b8926cf2a0d7f058ea0ffd45
jtasoftware <70141402+jtasoftware@users.noreply.github.com>
2026-09-19 14:34:18 +0200

    Fixed HomeKit version

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 export const PLATFORM_NAME = "homebridge-pichler-pkom4";
 export const PLUGIN_NAME = "homebridge-pichler-pkom4";
-export const PLUGIN_VERSION = "1.1.4";
+export const PLUGIN_VERSION = "1.1.11";
 export const MANUFACTURER_NAME = "Pichler";
 export const APP_MATCHING_ID = "1484302121";
